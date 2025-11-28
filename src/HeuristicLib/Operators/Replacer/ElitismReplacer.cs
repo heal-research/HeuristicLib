bf303827a28fece587e4f8d85045baf15137fc89
Bernhard Werth <bernhard.werth@fh-hagenberg.at>
2025-11-28 08:42:25 +0100

    Guard ElitismReplacer against invalid populationsizes

--- src/HeuristicLib/Operators/Replacer/ElitismReplacer.cs
+++ src/HeuristicLib/Operators/Replacer/ElitismReplacer.cs
@@ -7,22 +7,14 @@ public class ElitismReplacer<TGenotype> : Replacer<TGenotype> {
   public int Elites { get; }
 
   public ElitismReplacer(int elites) {
-    if (elites < 0)
-      throw new ArgumentOutOfRangeException(nameof(elites), "Elites must be non-negative.");
+    ArgumentOutOfRangeException.ThrowIfNegative(elites);
     Elites = elites;
   }
 
   public override IReadOnlyList<ISolution<TGenotype>> Replace(IReadOnlyList<ISolution<TGenotype>> previousPopulation, IReadOnlyList<ISolution<TGenotype>> offspringPopulation, Objective objective, IRandomNumberGenerator random) {
-    if (Elites < 0)
-      throw new ArgumentOutOfRangeException(nameof(Elites), "Elites must be non-negative.");
-
-    var elitesPopulation = previousPopulation
-                           .OrderBy(p => p.ObjectiveVector, objective.TotalOrderComparer)
-                           .Take(Elites);
-
-    return elitesPopulation
-           .Concat(offspringPopulation) // requires that offspring population size is correct
-           .ToArray();
+    var elitesPopulation = previousPopulation.OrderBy(p => p.ObjectiveVector, objective.TotalOrderComparer).Take(Elites);
+    var nonElites = offspringPopulation.Take(previousPopulation.Count - Math.Min(previousPopulation.Count, Elites));
+    return elitesPopulation.Concat(nonElites).ToArray();
   }
 
   public override int GetOffspringCount(int populationSize) => populationSize - Elites;
