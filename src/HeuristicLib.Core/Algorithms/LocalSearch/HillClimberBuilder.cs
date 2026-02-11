a01b9ec223d8efca7784b47e3f91b388325c703d
Bernhard Werth <bernhard.werth@fh-hagenberg.at>
2026-02-11 15:09:19 +0100

    remove duplicate operators in hill climber

--- src/HeuristicLib.Core/Algorithms/LocalSearch/HillClimberBuilder.cs
+++ src/HeuristicLib.Core/Algorithms/LocalSearch/HillClimberBuilder.cs
@@ -19,10 +19,6 @@ public record HillClimberBuilder<TG, TS, TP>
   public required IMutator<TG, TS, TP> Mutator { get; set; }
   public required ICreator<TG, TS, TP> Creator { get; set; }
 
-  public IEvaluator<TG, TS, TP> Evaluator { get; set; } = new DirectEvaluator<TG>();
-  public ITerminator<TG, SingleSolutionState<TG>, TS, TP> Terminator { get; set; } = new AfterIterationsTerminator<TG>(100);
-  public IInterceptor<TG, SingleSolutionState<TG>, TS, TP>? Interceptor { get; set; } = null;
-
   public override HillClimberBuildSpec<TG, TS, TP> CreateBuildSpec() => new(
     Evaluator, Terminator, Interceptor, MaxNeighbors, BatchSize, Direction, Mutator, Creator
   );
