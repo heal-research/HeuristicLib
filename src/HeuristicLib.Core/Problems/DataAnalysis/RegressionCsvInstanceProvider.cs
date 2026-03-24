c8e638718eeb42673da031da6648f00d618d8be6
Florian Bachinger <flo.bachinger@gmail.com>
2026-03-24 07:13:58 +0000

    add overload for ImportData to handle both, int trainingRowCount and double trainingSplit

--- src/HeuristicLib.Core/Problems/DataAnalysis/RegressionCsvInstanceProvider.cs
+++ src/HeuristicLib.Core/Problems/DataAnalysis/RegressionCsvInstanceProvider.cs
@@ -5,20 +5,35 @@ namespace HEAL.HeuristicLib.Problems.DataAnalysis;
 
 public static class RegressionCsvInstanceProvider
 {
+  public static RegressionProblemData ImportData(string path, int trainingRowCount)
+  {
+    var csvFileParser = ParseFile(path);
+    return ImportFromParser(csvFileParser, trainingRowCount);
+  }
+
   public static RegressionProblemData ImportData(string path, double trainingSplit = 0.66)
+  {
+    var csvFileParser = ParseFile(path);
+    return ImportFromParser(csvFileParser, (int)(csvFileParser.Rows * trainingSplit));
+  }
+
+  private static TableFileParser ParseFile(string path)
   {
     var csvFileParser = new TableFileParser();
     csvFileParser.Parse(path, csvFileParser.AreColumnNamesInFirstLine(path));
+    return csvFileParser;
+  }
 
+  private static RegressionProblemData ImportFromParser(TableFileParser csvFileParser, int trainingRowCount)
+  {
     Dataset dataset = new ModifiableDataset(csvFileParser.VariableNames, csvFileParser.Values);
     var targetVar = dataset.DoubleVariables.Last();
 
     // turn off input variables that are constant in the training partition
-    var trainingEnd = (int)(csvFileParser.Rows * trainingSplit);
-    var allowedInputVars = csvFileParser.Rows >= 3 ? dataset.DoubleVariables.Where(variableName => dataset.GetDoubleValues(variableName, Enumerable.Range(0, trainingEnd)).Range() > 0 && variableName != targetVar).ToList() : dataset.DoubleVariables.Where(x => !x.Equals(targetVar)).ToList();
+    var allowedInputVars = csvFileParser.Rows >= 3
+      ? dataset.DoubleVariables.Where(v => dataset.GetDoubleValues(v, Enumerable.Range(0, trainingRowCount)).Range() > 0 && v != targetVar).ToList()
+      : dataset.DoubleVariables.Where(v => v != targetVar).ToList();
 
-    var regressionData = new RegressionProblemData(dataset, targetVar, allowedInputVars, ..trainingEnd);
-
-    return regressionData;
+    return new RegressionProblemData(dataset, targetVar, allowedInputVars, ..trainingRowCount);
   }
 }
