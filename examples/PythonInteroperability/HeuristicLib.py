77e029c0bfae183349e4267b51498c7ba1608af1
Bernhard Werth <bernhard.werth@fh-hagenberg.at>
2026-03-25 13:07:02 +0100

    worked on examples

--- examples/PythonInteroperability/HeuristicLib.py
+++ examples/PythonInteroperability/HeuristicLib.py
@@ -11,4 +11,28 @@ dll_dir = os.path.join(_here, "csProject", "bin", "Release", "net10.0", "publish
 if dll_dir not in sys.path:
     sys.path.append(dll_dir)
 
-clr.AddReference("HEAL.HeuristicLib.Core")
+clr.AddReference("HEAL.HeuristicLib.Core") # type: ignore # noqa: E1101
+
+from System import Array, Double, Int32  # type: ignore  # noqa: E402 # noqa: E0401
+
+def double_array(x):
+    return Array[Double](x)
+
+def int_array(x):
+    return Array[Int32](x)
+
+def double_array_2d(x):
+    rows = len(x)
+    cols = len(x[0]) if rows > 0 else 0
+
+    arr = Array.CreateInstance(Double, rows, cols)
+
+    for i in range(rows):
+        if len(x[i]) != cols:
+            raise ValueError("All rows must have the same length")
+        for j in range(cols):
+            arr[i, j] = float(x[i][j])
+
+    return arr
+
+__all__ = ["double_array", "int_array","double_array_2d"]
